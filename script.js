385d4e4a432d36879d5655e4b7824d6a3b5acf09
Sumon <sumonkhan1081@gmail.com>
2023-08-12 00:42:40 +0530

    page3 done

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
+function loco(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+loco();
+
+
+function page1Andpage2(){
+    
 gsap.to(".page1>.page1-img", {
     width: '100%',
     height: '100vh',
     scrollTrigger: {
         trigger: ".page1",
-        scroller: "body",
+        scroller: ".main",
         // markers:true,
         start: "top 10%",
         end: "top -40%",
@@ -12,16 +48,51 @@ gsap.to(".page1>.page1-img", {
     }
 })
 gsap.from(".page2>h1", {
-    rotate:'5',
+    rotate: '5',
     opacity: 0,
     y: 100,
-    stagger:1,
+    stagger: 1,
     scrollTrigger: {
         trigger: ".page2>h1",
-        scroller: "body",
+        scroller: ".main",
         // markers: true,
-        start:"top 60%",
-        end:"top 40%",
-        scrub:3,
+        start: "top 60%",
+        end: "top 40%",
+        scrub: 3,
     }
 })
+}
+page1Andpage2();
+
+//page3 text animation with timeline
+function page3Animation(){
+    let page3TL = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".page3",
+            scroller: ".main",
+            pin: true,
+            scrub: 2,
+            // markers: true,
+            start: "top 0%",
+            end: "top -100%",
+        }
+    });
+    
+    
+    page3TL
+        .from(".page3 h1", {
+            scale: 1.95,
+            lineHeight: "20vw"
+        }, "a")
+        .from(".page3 h2", {
+            scale: 1.9,
+            lineHeight: "35vw"
+        }, "a")
+        .to(".page4", {
+            y: "-180vh"
+        }, "a")
+    
+}
+page3Animation();
+
+
